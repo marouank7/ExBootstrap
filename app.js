9e2579ebc566880fd6500a2aa1dcb75341321fad
Ktb <marouan_k@live.be>
2020-06-17 14:54:18 +0200

    réalisation de la navbar et ses effet, partie OurProducts, Medias

---
+++ app.js
@@ -0,0 +1,19 @@
+// attach a callback function to window scroll event
+window.onscroll = function() {toggleNavClassOnScroll()};
+
+// get a ref to the navbar in memory
+let navbar = document.getElementById("navbar");
+
+
+function toggleNavClassOnScroll() {
+  // apply a style class based on the current state of the scrolling
+  if (window.pageYOffset > 0) {
+    navbar.classList.remove("navbar-light");
+    navbar.classList.add("bg-dark");
+    navbar.classList.add("navbar-dark");
+  } else {
+    navbar.classList.remove("bg-dark");
+    navbar.classList.remove("navbar-dark");
+    navbar.classList.add("navbar-light");
+  }
+}
